bcb0bd0a8e216add144a1a8c304ce11ea127ea27
Kate <katensullivan55@gmail.com>
2022-07-19 17:59:27 -0400

    get one or all product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,33 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const pData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }],
+    });
+    res.status(200).json(pData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  try {
+    const pData = await Product.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [
+        { model: Category, attributes: ['category_name'] },
+        { model: Tag, attributes: ['tag_name'] },
+      ],
+    });
+    res.json(pData);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 // create new product
